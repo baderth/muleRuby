5b268778531198b6c65892f9f6bb92b0ac546032
Thomas Bader <th.bader@gmail.com>
2012-07-18 12:50:51 +0200

    initial commit

---
+++ rubyComponent.rb
@@ -0,0 +1,4 @@
+puts "Hello from 1st ruby script!\n"
+puts $payload
+puts "\n"
+payload = "This is the new payload, forget the first one"
---
+++ rubyComponent2.rb
@@ -0,0 +1,2 @@
+puts "This is the 2nd ruby script, here is my payload:\n"
+puts $payload
